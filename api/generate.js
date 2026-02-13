7b54406c5c67b47013a0f877c72b357500ae5b5f
Infinite-AIs <sw.ventures.12@gmail.com>
2026-02-13 16:59:35 -0500

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -1,1 +1,22 @@
+export default async function handler(req, res) {
+  const { prompt } = req.body;
 
+  const response = await fetch(
+    "https://api-inference.huggingface.co/models/facebook/musicgen-small",
+    {
+      method: "POST",
+      headers: {
+        "Authorization": `Bearer ${process.env.HF_TOKEN}`,
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        inputs: prompt
+      })
+    }
+  );
+
+  const arrayBuffer = await response.arrayBuffer();
+
+  res.setHeader("Content-Type", "audio/wav");
+  res.send(Buffer.from(arrayBuffer));
+}
